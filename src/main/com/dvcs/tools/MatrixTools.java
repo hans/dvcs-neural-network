f4a885eba36884a4cd08818e4badb682ff1b66a0
Hans Engel <engel@engel.uk.to>
2013-02-20 18:35:10 -0700

    Remove unused matrixLogarithm

--- src/main/com/dvcs/tools/MatrixTools.java
+++ src/main/com/dvcs/tools/MatrixTools.java
@@ -3,7 +3,6 @@ package com.dvcs.tools;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 
-
 public class MatrixTools {
 
 	/**
@@ -11,13 +10,13 @@ public class MatrixTools {
 	 */
 	public static RealMatrix matrixSigmoid(RealMatrix z) {
 		z = z.copy();
-	
+
 		for (int i = 0; i < z.getRowDimension(); i++) {
 			for (int j = 0; j < z.getColumnDimension(); j++) {
 				z.setEntry(i, j, MatrixTools.sigmoid(z.getEntry(i, j)));
 			}
 		}
-	
+
 		return z;
 	}
 
@@ -27,13 +26,13 @@ public class MatrixTools {
 
 	public static double matrixSum(RealMatrix m) {
 		double sum = 0;
-	
+
 		for (int i = 0; i < m.getRowDimension(); i++) {
 			for (int j = 0; j < m.getColumnDimension(); j++) {
 				sum += m.getEntry(i, j);
 			}
 		}
-	
+
 		return sum;
 	}
 
@@ -42,30 +41,15 @@ public class MatrixTools {
 	 */
 	public static RealMatrix squareMatrixElements(RealMatrix m) {
 		m = m.copy();
-	
+
 		for (int i = 0; i < m.getRowDimension(); i++) {
 			for (int j = 0; j < m.getColumnDimension(); j++) {
 				double val = m.getEntry(i, j);
 				m.setEntry(i, j, val * val);
 			}
 		}
-	
-		return m;
-	}
 
-	/**
-	 * Replace each element $x$ of a matrix with $\log(x)$.
-	 */
-	static RealMatrix matrixLogarithm(RealMatrix z) {
-		z = z.copy();
-	
-		for (int i = 0; i < z.getRowDimension(); i++) {
-			for (int j = 0; j < z.getColumnDimension(); j++) {
-				z.setEntry(i, j, Math.log(z.getEntry(i, j)));
-			}
-		}
-	
-		return z;
+		return m;
 	}
 
 	/**
@@ -74,14 +58,14 @@ public class MatrixTools {
 	 */
 	public static RealMatrix randomMatrix(int m, int n, double min, double max) {
 		RealMatrix ret = new Array2DRowRealMatrix(m, n);
-	
+
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
 				double val = Math.random() * (max - min) + min;
 				ret.setEntry(i, j, val);
 			}
 		}
-	
+
 		return ret;
 	}
 
@@ -90,18 +74,18 @@ public class MatrixTools {
 	 */
 	public static double[] unroll(RealMatrix m) {
 		double[] ret = new double[m.getRowDimension() * m.getColumnDimension()];
-		
-		for ( int i = 0; i < m.getRowDimension(); i++ ) {
-			for ( int j = 0; j < m.getColumnDimension(); j++ ) {
+
+		for (int i = 0; i < m.getRowDimension(); i++) {
+			for (int j = 0; j < m.getColumnDimension(); j++) {
 				ret[i * m.getRowDimension() + j] = m.getEntry(i, j);
 			}
 		}
-		
+
 		return ret;
 	}
 
 	/**
-	 * Unroll a vector into a square matrix. The vector must have a length that
+	 * Reshape a vector into a square matrix. The vector must have a length that
 	 * is a perfect square.
 	 */
 	public static RealMatrix reshape(double[] row) {
@@ -110,16 +94,16 @@ public class MatrixTools {
 			throw new RuntimeException(
 					"Reshape method provided with a vector whose length is not a square number");
 		}
-		
+
 		int length = (int) sq;
 		RealMatrix ret = new Array2DRowRealMatrix(length, length);
-		
-		for ( int i = 0; i < length; i++ ) {
-			for ( int j = 0; j < length; j++ ) {
+
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
 				ret.setEntry(i, j, row[i * length + j]);
 			}
 		}
-		
+
 		return ret;
 	}
 
